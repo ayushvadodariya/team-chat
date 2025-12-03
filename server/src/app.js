d713de8fce3c8567a808065803aa8c23a6e4057d
ayush <ayushvadodariya12@gmail.com>
2025-12-03 16:19:24 +0530

    chore: initial project scaffold

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
