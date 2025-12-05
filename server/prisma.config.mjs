3fe9609a747bcacc37febd4d284b9946ee12958f
ayush <ayushvadodariya12@gmail.com>
2025-12-05 11:49:10 +0530

    format code

--- server/prisma.config.mjs
+++ server/prisma.config.mjs
@@ -1,9 +1,9 @@
 export default {
-    prisma: {
-        schema: 'prisma/schema.prisma',
-    },
-    datasource: {
-        provider: 'postgresql',
-        url: process.env.DATABASE_URL,
-    },
-}
+  prisma: {
+    schema: 'prisma/schema.prisma',
+  },
+  datasource: {
+    provider: 'postgresql',
+    url: process.env.DATABASE_URL,
+  },
+};
