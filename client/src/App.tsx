1065e6cbfffdcd11493554c286192b72eb3efd5c
ayush <ayushvadodariya12@gmail.com>
2025-12-05 18:23:57 +0530

    chore(client): init react-ts with tailwind setup.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen w-full">
+      <h1 className="text-3xl font-bold">
+        Team Chat
+      </h1>
+    </div>
+  )
+}
+
+export default App
